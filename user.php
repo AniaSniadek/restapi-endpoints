db9b6139e4de55aaafd9e155e9e1bc0917becacb
Speedy09 <sniadek97@gmail.com>
2020-04-09 19:48:02 +0200

    read in user

--- user.php
+++ user.php
@@ -2,5 +2,26 @@
 
 class User
 {
-    
+    private $conn;
+    private $table_name = 'users';
+
+    public $id;
+    public $name;
+    public $email;
+    public $password;
+
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    function read()
+    {
+        $query = "SELECT id, name, email, password FROM " . $this->table_name . "";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+
+        return $stmt;
+    }
 }
